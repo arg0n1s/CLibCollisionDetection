be0d514ba30ed1732c7f69c8bf3a0777d7c9c618
arg0n1s <basti_seppl@yahoo.com>
2017-12-15 11:07:11 +0100

    changed to x64 and began working on VTKVisualization module

---
+++ VTKVisualizationModule/include/VTKVisualization.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <vtk-8.0\vtkSmartPointer.h>
+#include <vtk-8.0\vtkRenderer.h>
+#include <vtk-8.0\vtkRenderWindow.h>
+#include <vtk-8.0\vtkRenderWindowInteractor.h>
+
+/*
+#include <vtk-8.0\vtkCylinderSource.h>
+#include <vtk-8.0\vtkSphereSource.h>
+#include <vtk-8.0\vtkPolyDataMapper.h>
+#include <vtk-8.0\vtkActor.h>
+
+#include <vtk-8.0\vtkVertexGlyphFilter.h>
+#include <vtk-8.0\vtkProperty.h>
+#include <vtk-8.0\vtkAxesActor.h>
+#include <vtk-8.0\vtkTransform.h>
+#include <vtk-8.0\vtkPointData.h>
+*/
+
+namespace vis {
+
+	// For compatibility with new VTK generic data arrays
+#ifdef vtkGenericDataArray_h
+#define InsertNextTupleValue InsertNextTypedTuple
+#endif
+
+	class VTKVisualization {
+	public:
+		VTKVisualization();
+	private:
+		vtkSmartPointer<vtkRenderer> renderer;
+		vtkSmartPointer<vtkRenderWindow> renderWindow;
+		vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor;
+
+		void createRenderer();
+
+		void createRenderWindow();
+
+		void createRenderWindowInteractor();
+
+	};
+
+
+}
---
+++ VTKVisualizationModule/src/VTKVisualization.cpp
@@ -0,0 +1,37 @@
+#include "..\include\VTKVisualization.h"
+
+namespace vis {
+
+	// For compatibility with new VTK generic data arrays
+#ifdef vtkGenericDataArray_h
+#define InsertNextTupleValue InsertNextTypedTuple
+#endif
+
+	VTKVisualization::VTKVisualization() {
+		createRenderer();
+		createRenderWindow();
+		createRenderWindowInteractor();
+	}
+
+	void VTKVisualization::createRenderer()
+	{
+		renderer = vtkSmartPointer<vtkRenderer>::New();
+		renderer->SetBackground(0.2, 0.2, 0.2);
+	}
+
+	void VTKVisualization::createRenderWindow()
+	{
+		renderWindow =
+			vtkSmartPointer<vtkRenderWindow>::New();
+		renderWindow->AddRenderer(renderer);
+	}
+
+	void VTKVisualization::createRenderWindowInteractor()
+	{
+		renderWindowInteractor =
+			vtkSmartPointer<vtkRenderWindowInteractor>::New();
+		renderWindowInteractor->SetRenderWindow(renderWindow);
+	}
+
+
+}
